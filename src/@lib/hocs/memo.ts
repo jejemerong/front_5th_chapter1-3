a861a57e7e69b43c888e88cd5be0d9b250e6c154
jejemerong <9708jjw@gmail.com>
2025-04-09 11:08:31 +0900

    feat: hook, hoc 추가

--- src/@lib/hocs/memo.ts
+++ src/@lib/hocs/memo.ts
@@ -1,10 +1,29 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, { ComponentType } from "react"; // TODO: forwardRef
 import { shallowEquals } from "../equalities";
-import { ComponentType } from "react";
+import { useRef } from "../hooks";
 
 export function memo<P extends object>(
   Component: ComponentType<P>,
   _equals = shallowEquals,
 ) {
-  return Component;
+  return function MemoizedComponent(props: P) {
+    const prevPropsRef = useRef<P | null>(null); // 이전 props 를 저장할 ref
+    const componentRef = useRef<ComponentType<P> | null>(null); // 메모이제이션된 컴포넌트를 저장할 ref
+
+    // 이전 props 가 존재하고 현재 props 와 같다면 메모이제이션된 컴포넌트 반환
+    if (
+      prevPropsRef.current !== undefined &&
+      _equals(prevPropsRef.current, props)
+    ) {
+      return componentRef.current;
+    }
+
+    // 컴포넌트 업데이트 확인
+    if (!_equals(prevPropsRef.current, props)) {
+      prevPropsRef.current = props;
+      componentRef.current = Component;
+    }
+
+    return React.createElement(componentRef.current || Component, { ...props });
+  };
 }
--- src/@lib/hooks/useMemo.ts
+++ src/@lib/hooks/useMemo.ts
@@ -1,12 +1,33 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { DependencyList } from "react";
 import { shallowEquals } from "../equalities";
+import { useRef } from "./useRef";
 
 export function useMemo<T>(
   factory: () => T,
   _deps: DependencyList,
   _equals = shallowEquals,
 ): T {
-  // 직접 작성한 useRef를 통해서 만들어보세요.
-  return factory();
+  // 1. 이전 의존성과 결과를 저장할 ref 생성
+  const depsRef = useRef<DependencyList | null>(null);
+  const valueRef = useRef<T | null>(null);
+
+  // 2. 현재 의존성과 이전 의존성 비교
+  const isSame = depsRef.current !== null && _equals(depsRef.current, _deps);
+
+  // 3. 의존성이 변경되었거나 최초 렌더링인 경우만 factory 함수 실행
+  // 렌더링 시점에 if 문 실행, deps 는 같지만
+  // mockFactory() 는 반환값이 undefined 이기 때문에 valueRef.current 는 항상 undefined 일 수 밖에 없다.
+  if (
+    !isSame ||
+    (valueRef.current === undefined && depsRef.current === undefined)
+  ) {
+    // 값 메모이제이션
+    valueRef.current = factory();
+    // 현재 의존성 저장
+    depsRef.current = _deps;
+    console.log("depsRef.current", depsRef.current);
+  }
+
+  // 4. 메모이제이션된 값 반환
+  return valueRef.current as T;
 }
